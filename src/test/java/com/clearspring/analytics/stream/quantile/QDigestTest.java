523fb751e9e8dfba02d8388226b974a0706a5ff6
Eugene Kirpichov <ekirpichov@gmail.com>
2012-05-04 23:05:28 +0400

    Added error bound checking in test. Fixed documentation (error bound was incorrect).

--- src/test/java/com/clearspring/analytics/stream/quantile/QDigestTest.java
+++ src/test/java/com/clearspring/analytics/stream/quantile/QDigestTest.java
@@ -4,71 +4,30 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+
 public class QDigestTest {
     @Test
-    public void testOnExampleFromPaper() {
-	QDigest digest = new QDigest(3);
-
-	digest.offer(1);
-	printRanges(digest);
-	digest.offer(3);
-	printRanges(digest);
-	digest.offer(3);
-	printRanges(digest);
-	digest.offer(3);
-	printRanges(digest);
-	digest.offer(3);
-	printRanges(digest);
-	digest.offer(4);
-	printRanges(digest);
-	digest.offer(4);
-	printRanges(digest);
-	digest.offer(4);
-	printRanges(digest);
-	digest.offer(4);
-	printRanges(digest);
-	digest.offer(4);
-	printRanges(digest);
-	digest.offer(4);
-	printRanges(digest);
-	digest.offer(5);
-	printRanges(digest);
-	digest.offer(6);
-	printRanges(digest);
-	digest.offer(7);
-	printRanges(digest);
-	digest.offer(8);
-
-	printRanges(digest);
-
-	System.out.println(digest.getQuantile(0.25));
-	System.out.println(digest.getQuantile(0.5));
-	System.out.println(digest.getQuantile(0.75));
-
-    }
-
-    @Test
     public void testOnSquares() {
-	QDigest digest = new QDigest(1000);
+	double compressionFactor = 1000;
+	QDigest digest = new QDigest(compressionFactor);
+	int max = 100000;
 	for(int j = 0; j < 10; ++j) {
-	    for(long i = 0; i < 100000; ++i) {
+	    for(long i = 0; i < max; ++i) {
 		digest.offer(i*i);
 	    }
 	}
-	// TODO actually test accuracy.
-	// Check: actual rank of item returned by getQuantile(i) is i*(1-eps) .. i*(1+eps),
-	// where eps = logCapacity/compressionFactor.
-	for(double i = 0; i < 1; i += 0.1)
-	    System.out.println(i + " => " + digest.getQuantile(i) + " -- " + (i*10000L)*(i*10000L));
-    }
+	int logCapacity = 1;
+	for(long scale = 1; scale < max; scale *= 2, logCapacity++)
+	    ;
+	double eps = logCapacity/compressionFactor;
 
-    private void printRanges(QDigest digest) {
-	List<long[]> ranges = digest.toAscRanges();
-	StringBuilder sb = new StringBuilder();
-	for(long[] range : ranges) {
-	    if(sb.length() > 0) sb.append("; ");
-	    sb.append(range[0] + ".." + range[1] + " ==> " + range[2]);
+	for(double i = 0; i < 1; i += 0.1) {
+	    double answer = digest.getQuantile(i);
+	    System.out.println(i + " => " + answer + " -- " + (i*max)*(i*max));
+	    double actualRank = Math.min(1.0, Math.sqrt(answer)/max);
+	    assertTrue(actualRank + " not in " + (i-eps) + " .. " + (i+eps),
+		    actualRank > i - eps && actualRank < i + eps);
 	}
-	System.out.println(sb.toString());
     }
 }
