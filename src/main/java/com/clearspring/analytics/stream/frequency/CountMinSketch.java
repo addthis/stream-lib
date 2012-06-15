87a0cde639eb0d7bd288942cec23e03ca05193ba
Eugene Kirpichov <ekirpichov@gmail.com>
2012-06-15 11:47:20 -0700

    Expand * imports

--- src/main/java/com/clearspring/analytics/stream/frequency/CountMinSketch.java
+++ src/main/java/com/clearspring/analytics/stream/frequency/CountMinSketch.java
@@ -1,6 +1,10 @@
 package com.clearspring.analytics.stream.frequency;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.Random;
 
 /**
