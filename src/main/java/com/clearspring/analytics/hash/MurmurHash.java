48a0c08a16a5a37e0bdd2a0b5afc8d416e5d1230
Matt Abrams <abramsm@addthis.com>
2013-01-24 14:52:24 -0500

    update murmurhash to include 64 bit capability

--- src/main/java/com/clearspring/analytics/hash/MurmurHash.java
+++ src/main/java/com/clearspring/analytics/hash/MurmurHash.java
@@ -132,4 +132,81 @@ public class MurmurHash
 
         return h;
     }
+
+	public static long hash64(Object o)
+	{
+		if (o == null)
+		{
+			return 0l;
+		}
+		else if (o instanceof String)
+		{
+			final byte[] bytes = ((String)o).getBytes();
+			return hash64(bytes, bytes.length);
+		}
+		return hash64(o.toString());
+	}
+
+	// 64 bit implementation copied from here:  https://github.com/tnm/murmurhash-java
+
+	/**
+	 * Generates 64 bit hash from byte array with default seed value.
+	 *
+	 * @param data byte array to hash
+	 * @param length length of the array to hash
+	 * @return 64 bit hash of the given string
+	 */
+	public static long hash64( final byte[] data, int length) {
+		return hash64(data, length, 0xe17a1465);
+	}
+
+
+	/**
+	 * Generates 64 bit hash from byte array of the given length and seed.
+	 *
+	 * @param data byte array to hash
+	 * @param length length of the array to hash
+	 * @param seed initial seed value
+	 * @return 64 bit hash of the given array
+	 */
+	public static long hash64( final byte[] data, int length, int seed) {
+		final long m = 0xc6a4a7935bd1e995L;
+		final int r = 47;
+
+		long h = (seed&0xffffffffl)^(length*m);
+
+		int length8 = length/8;
+
+		for (int i=0; i<length8; i++) {
+			final int i8 = i*8;
+			long k =  ((long)data[i8+0]&0xff)      +(((long)data[i8+1]&0xff)<<8)
+					+(((long)data[i8+2]&0xff)<<16) +(((long)data[i8+3]&0xff)<<24)
+					+(((long)data[i8+4]&0xff)<<32) +(((long)data[i8+5]&0xff)<<40)
+					+(((long)data[i8+6]&0xff)<<48) +(((long)data[i8+7]&0xff)<<56);
+
+			k *= m;
+			k ^= k >>> r;
+			k *= m;
+
+			h ^= k;
+			h *= m;
+		}
+
+		switch (length%8) {
+			case 7: h ^= (long)(data[(length&~7)+6]&0xff) << 48;
+			case 6: h ^= (long)(data[(length&~7)+5]&0xff) << 40;
+			case 5: h ^= (long)(data[(length&~7)+4]&0xff) << 32;
+			case 4: h ^= (long)(data[(length&~7)+3]&0xff) << 24;
+			case 3: h ^= (long)(data[(length&~7)+2]&0xff) << 16;
+			case 2: h ^= (long)(data[(length&~7)+1]&0xff) << 8;
+			case 1: h ^= (long)(data[length&~7]&0xff);
+				h *= m;
+		};
+
+		h ^= h >>> r;
+		h *= m;
+		h ^= h >>> r;
+
+		return h;
+	}
 }
