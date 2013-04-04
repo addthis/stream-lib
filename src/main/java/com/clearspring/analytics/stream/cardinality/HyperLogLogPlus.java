b5878027833e677e234e03ab07ad760bfe527551
Matt Abrams <abramsm@addthis.com>
2013-04-04 14:53:54 -0400

    refs #21 - a bit hokey but the threshold data just seems flat wrong so I've modified to provide more reasonable results.  very likely requires more detailed analysis

--- src/main/java/com/clearspring/analytics/stream/cardinality/HyperLogLogPlus.java
+++ src/main/java/com/clearspring/analytics/stream/cardinality/HyperLogLogPlus.java
@@ -49,7 +49,7 @@ public class HyperLogLogPlus implements ICardinality
     }
 
     // threshold and bias data taken from google's bias correction data set:  https://docs.google.com/document/d/1gyjfMHy43U9OWBXxfaeG-3MjGzejW1dlpyMwEYAAWEI/view?fullscreen#
-    double[] thresholdData = {10, 20, 40, 80, 220, 400, 900, 1800, 3100, 6500, 11500, 20000, 500000, 120000, 350000};
+    double[] thresholdData = {10, 20, 40, 80, 220, 400, 900, 1800, 3100, 6500, 15500, 20000, 500000, 120000, 350000};
 
     double[][] rawEstimateData = {
             // precision 4
@@ -461,7 +461,7 @@ public class HyperLogLogPlus implements ICardinality
                 {
                     H = estimatePrime;
                 }
-                if (H < thresholdData[p])
+		if (H < thresholdData[p-4])
                 {
                     return Math.round(H);
                 }
