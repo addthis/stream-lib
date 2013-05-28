9a5d66894c19cfc6ade2738935b075dd538acd50
cykl <clement@unportant.info>
2013-05-28 22:47:03 +0200

    Remove unused function: Bits.getBits(byte[] mBytes)

--- src/main/java/com/clearspring/analytics/util/Bits.java
+++ src/main/java/com/clearspring/analytics/util/Bits.java
@@ -16,27 +16,12 @@
 
 package com.clearspring.analytics.util;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public class Bits
 {
 
     public static final int INT_BYTES = Integer.SIZE / 8;
-
-    public static int[] getBits(byte[] mBytes) throws IOException
-    {
-        int bitSize = mBytes.length / 4;
-        int[] bits = new int[bitSize];
-        DataInputStream dis = new DataInputStream(new ByteArrayInputStream(mBytes));
-        for (int i = 0; i < bitSize; i++)
-        {
-            bits[i] = dis.readInt();
-        }
-        return bits;
-    }
 
     public static int [] getBits(ByteBuffer bb, int size)
     {
