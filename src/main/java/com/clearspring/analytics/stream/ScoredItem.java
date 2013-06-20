f88f6368fe95c65da98619b8926f1e083a8cc4ee
Matt Abrams <abramsm@addthis.com>
2013-06-20 06:20:27 -0700

    add missing imports

--- src/main/java/com/clearspring/analytics/stream/ScoredItem.java
+++ src/main/java/com/clearspring/analytics/stream/ScoredItem.java
@@ -16,6 +16,9 @@
 
 package com.clearspring.analytics.stream;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * @author Eric Vlaanderen
  */
