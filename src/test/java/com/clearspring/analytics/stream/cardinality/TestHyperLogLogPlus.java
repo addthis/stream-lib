f59d7f4574b21554be12274c06a7439f8a46f266
Marcus Eriksson <marcuse@apache.org>
2016-03-10 09:14:50 +0100

    remove test from test method name..

--- src/test/java/com/clearspring/analytics/stream/cardinality/TestHyperLogLogPlus.java
+++ src/test/java/com/clearspring/analytics/stream/cardinality/TestHyperLogLogPlus.java
@@ -510,13 +510,13 @@ public class TestHyperLogLogPlus {
     }
 
     @Test
-    public void testOldDeserialization() throws IOException {
+    public void oldDeserialization() throws IOException {
         byte [] b = new byte[]{-1, -1, -1, -2, 14, 25, 0, 4, 25, -27, -1, -1, 15, -101, -128, -128, -16, 7, -27, -1, -1, -97, 8};
         HyperLogLogPlus hll = HyperLogLogPlus.Builder.build(b);
         assertEquals(hll.getRegisterSet().isDirect(), false);
     }
     @Test
-    public void testDeserializationWithOffHeap() throws IOException {
+    public void deserializationWithOffHeap() throws IOException {
         byte [] b = new byte[]{-1, -1, -1, -3, 14, 25, 0, 1, 4, 25, -27, -1, -1, 15, -101, -128, -128, -16, 7, -27, -1, -1, -97, 8};
         //                                  ^ version     ^ direct boolean
         HyperLogLogPlus hll = HyperLogLogPlus.Builder.build(b);
